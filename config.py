d96e94963d27b078ae009bfea7ddb465f36678d0
FimastGDx <fimastdasher@gmail.com>
2026-01-19 12:06:54 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+TELEGRAM_BOT_TOKEN = "***:***"
+GROQ_API_KEY = "***"
+PROXY_URL = "http://username:password@ip:port"
+RATE_LIMIT_SECONDS = 30
+ADMIN_USERNAME = "f1mastgd"
+
+# system
+MAIN_LOG = 'main.log'
+TABLE_JSON = 'table.json'
